239c40056d9909d87c48f538d117aecdb9ed9ae4
Zadkiel Aharonian <hello@zadkiel.fr>
2018-12-12 22:30:03 +0100

    fix

--- start.sh
+++ start.sh
@@ -5,25 +5,7 @@
 ########################################################################
 lib/envsubst < config/config-heroku-template.toml > config/config-heroku.toml
 
-#####################################
-# Pass SIGTERM to Matterbridge proc #
-#####################################
-function _term {
-  echo "Sending SIGTERM to matterbridge"
-
-  kill --TERM "$PID" 2>/dev/null
-}
-
-trap _term SIGTERM
-
-####################
+######################
 # Start Matterbridge #
-####################
-./matterbridge -conf=config/config-heroku.toml &
-
-PID=$!
-
-#####################################
-# Wait for this process to complete #
-#####################################
-wait "$PID"
+######################
+exec ./matterbridge -conf=config/config-heroku.toml
